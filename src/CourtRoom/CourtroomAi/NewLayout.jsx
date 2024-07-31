1a0ae1c47a48eb38ca9d48ec9d698055e7b18ea1
rajkbharali <rajkbharali@gmail.com>
2024-07-31 08:59:04 +0530

    new layout

--- src/CourtRoom/CourtroomAi/NewLayout.jsx
+++ src/CourtRoom/CourtroomAi/NewLayout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const NewLayout = () => {
-  return <div>NewLayout</div>;
+  return (
+    <div class="h-screen m-0 overflow-hidden">
+      <div class="flex flex-col h-screen">
+        <div class="h-[20vh] overflow-auto bg-red-300">
+          <p>Top Content</p>
+        </div>
+        <div class="flex-1 overflow-auto bg-blue-300">
+          <p>Middle Content</p>
+        </div>
+        <div class="h-[10vh] overflow-auto bg-green-300">
+          <p>Bottom Content</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewLayout;
