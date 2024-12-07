395af1e1aae60a2a0a1db1f29bbe9bb130810591
Shubham Prajapati <shubham528prajapati@gmail.com>
2024-12-07 12:55:31 +0530

    changes done

--- src/components/Pricing/BuyPlan.jsx
+++ src/components/Pricing/BuyPlan.jsx
@@ -47,7 +47,7 @@ const BuyPlan = () => {
         const { amount, id, currency } = result.data.razorpayOrder;
         const { _id } = result.data.createdOrder;
         const options = {
-          key: "rzp_test_UWcqHHktRV6hxM",
+          key: "rzp_live_vlDmt5SV4QPDhN",
           amount: String(amount),
           currency: currency,
           name: "CLAW LEGALTECH PRIVATE LIMITED",
